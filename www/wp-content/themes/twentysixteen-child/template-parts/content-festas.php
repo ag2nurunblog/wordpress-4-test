16e594707c77f8bbf80da9853d4ddf5d4b65689b
Rafael <rsilveira@conrad-caine.com>
2017-01-13 21:36:50 -0200

    add timber plugin

--- www/wp-content/themes/twentysixteen-child/template-parts/content-festas.php
+++ www/wp-content/themes/twentysixteen-child/template-parts/content-festas.php
@@ -16,17 +16,5 @@
     <?php twentysixteen_post_thumbnail(); ?>
 
     <div class="entry-content">
-        <?php
-            the_content();
-
-            wp_link_pages( array(
-                'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-                'after'       => '</div>',
-                'link_before' => '<span>',
-                'link_after'  => '</span>',
-                'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-                'separator'   => '<span class="screen-reader-text">, </span>',
-            ) );
-        ?>
     </div>
 </article>
